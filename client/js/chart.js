d1faed493a3ee9279f1ff95280ea48155d6cea3f
siddh4489 <siddh4489@gmail.com>
2018-04-03 15:02:04 +0530

    Update chart.js

--- client/js/chart.js
+++ client/js/chart.js
@@ -47,7 +47,7 @@ angular.module('nibs.chart', ['nibs.config'])
                 slantedText:true,
                 slantedTextAngle:90 // here you can even use 180
             },
-            width: 600,
+            width: 450,
             height: 400,
             //bar: {groupWidth: "30%"},
             legend: { position: "left" },

3ff6ec73888c6c5f7666958418663bb25612ebcc
siddh4489 <siddh4489@gmail.com>
2018-04-04 17:01:58 +0530

    Update chart.js

--- client/js/chart.js
+++ client/js/chart.js
@@ -92,7 +92,6 @@ angular.module('nibs.chart', ['nibs.config'])
        $scope.sfu = {'suser':$window.localStorage.getItem('sfuser'),'spassword':$window.localStorage.getItem('sfpassword')};
        
        Chart.getProjectChartList($scope.sfu).success(function(datalist) {
-            var projectType = ["Subscription","Deployment time reduction","Code Optimization","Config Cleanup","Data Purge","Platform Enhancements","UIPM","INC","Self Study"];  
 	    google.charts.load('current', {'packages':['corechart','corechart', 'bar']});
             google.charts.setOnLoadCallback(drawbarChart);
            
@@ -110,7 +109,8 @@ angular.module('nibs.chart', ['nibs.config'])
                 data.addColumn('number','INC');
                 data.addColumn('number','Self Study');
                
-               
+                var projectType = ["Subscription","Deployment time reduction","Code Optimization","Config Cleanup","Data Purge","Platform Enhancements","UIPM","INC","Self Study"];  
+
 				var names = [];
 				var index;
 				for(var i=0;i<datalist.length;i++)

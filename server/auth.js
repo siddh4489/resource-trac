3ce13c1765134e46d1ec7fe3c0db4901f6bfdb05
siddh4489 <siddh4489@gmail.com>
2018-04-18 15:34:44 +0530

    Update auth.js

--- server/auth.js
+++ server/auth.js
@@ -172,6 +172,7 @@ function sflogin(req, res, next) {
     ses.email=req.body.email;
     ses.password = req.body.password;	
     	
+    	
     var nforce = require('nforce');
     //var userName = config.api.userName,
     //var password = config.api.password;
@@ -196,7 +197,7 @@ org.authenticate({ username: creds.email, password: creds.password}, function(er
 		org.query({ query: userquery}, function(err, resp1){
 
 		      if(!err && resp1.records) {
-			      
+			      ses.uid = resp1.records[0].id;
 			      return res.send({'email':ses.email,'status':'1','token':org.oauth.access_token,'runninguser':resp1.records,'sfuser':creds.email,'sfpassword':creds.password});
 		      }else{
 			 //res.send({'status':'1','error':err.message});

1d75872c9175c231a550fc8066eb995b06e8019e
siddh4489 <siddh4489@gmail.com>
2018-04-19 16:20:53 +0530

    Update tasklist.js

--- client/js/tasklist.js
+++ client/js/tasklist.js
@@ -12,6 +12,17 @@ angular.module('nibs.tasklist', ['nibs.config'])
                     }
                 }
             })
+    
+            .state('app.skillsetlist', {
+                url: "/skillsetlist",
+                views: {
+                    'menuContent' :{
+                        templateUrl: "templates/skillsetlist.html",
+                        controller: "SkillsetListController"
+                    }
+                }
+            })
+    
     })
 
     // Services
@@ -19,6 +30,9 @@ angular.module('nibs.tasklist', ['nibs.config'])
         return {
             getTaskList: function(theTasklst) {
                 return $http.post($rootScope.server.url + '/tasklist/',theTasklst);
+            },
+            getSkillsetList: function(theSkillsetlst) {
+                return $http.post($rootScope.server.url + '/skillsetlist/',theSkillsetlst);
             }
         };
     })
@@ -27,9 +41,18 @@ angular.module('nibs.tasklist', ['nibs.config'])
     .controller('TaskListController', function ($scope,$rootScope, $window, $ionicPopup, Tasklist, User) {
         $rootScope.username = $window.localStorage.getItem('username');
 
-        $scope.claimlist = {};
+        $scope.tasklist = {};
         $scope.sfu = {'suser':$window.localStorage.getItem('sfuser'),'spassword':$window.localStorage.getItem('sfpassword'),'uid':$window.localStorage.getItem('uid')};
             Tasklist.getTaskList($scope.sfu).success(function(datalist) {
-                     $scope.claimlist = datalist;
+                     $scope.tasklist = datalist;
                 });
+  })
+
+.controller('SkillsetListController', function ($scope,$rootScope, $window, $ionicPopup, Tasklist, User) {
+        $rootScope.username = $window.localStorage.getItem('username');
+        $scope.skillsetlist = {};
+        $scope.sfu = {'suser':$window.localStorage.getItem('sfuser'),'spassword':$window.localStorage.getItem('sfpassword'),'uid':$window.localStorage.getItem('uid')};
+            Tasklist.getSkillsetList($scope.sfu).success(function(datalist) {
+                     $scope.skillsetlist = datalist;
+             });
   });

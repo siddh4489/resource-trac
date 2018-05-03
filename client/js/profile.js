52726685bc99bf98681bade9615a125d3efd9a2a
siddh4489 <siddh4489@gmail.com>
2018-05-03 15:21:29 +0530

    Update profile.js

--- client/js/profile.js
+++ client/js/profile.js
@@ -92,32 +92,7 @@ angular.module('nibs.profile', ['nibs.config'])
 
     //Controllers
     .controller('ProfileCtrl', function ($rootScope, $scope,$window, $state, Taskview, STATUS_LABELS, STATUS_DESCRIPTIONS) {
-    
-    
-    $(document).ready(function() {
-
-    var readURL = function(input) {
-        if (input.files && input.files[0]) {
-            var reader = new FileReader();
-
-            reader.onload = function (e) {
-                $('.profile-pic1').attr('src', e.target.result);
-                
-            }
-    
-            reader.readAsDataURL(input.files[0]);
-        }
-    }
-    
-
-    $(".file-upload").on('change', function(){
-        readURL(this);
-    });
-    
-    $(".upload-button").on('click', function() {
-       $(".file-upload").click();
-    });
-});
+   
     
     $scope.sfu = {'date':'','suser':$window.localStorage.getItem('sfuser'),'spassword':$window.localStorage.getItem('sfpassword'),'uid':$window.localStorage.getItem('uid')};
 
